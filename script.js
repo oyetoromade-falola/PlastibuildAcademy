841bdf943599aa90bb3b8d31ead3e3ac138d86ce
Falola Oyetoromade Regina <falolaoyetoromade@gmail.com>
2026-07-05 21:10:22 +0100

    Implement counter animation in script.js
    Add counter animation functionality to update displayed numbers.

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 });
+
+
+
+// COUNTER ANIMATION
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (current < target) {
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
+
+        

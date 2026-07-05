1aba9bd549c7325d20b04ebaa5aa50372861b9ee
Falola Oyetoromade Regina <falolaoyetoromade@gmail.com>
2026-07-05 22:23:10 +0100

    Implement counter increment on DOMContentLoaded
    Add counter functionality that increments to a target value.

--- script.js
+++ script.js
@@ -80,3 +80,28 @@ filterButtons.forEach(button => {
 });
 
 
+
+            document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll(".counter");
+
+    counters.forEach(counter => {
+        counter.innerText = "0";
+
+        const update = () => {
+            const target = +counter.getAttribute("data-target");
+            const current = +counter.innerText;
+
+            const increment = target / 100;
+
+            if (current < target) {
+                counter.innerText = Math.ceil(current + increment);
+                setTimeout(update, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        update();
+    });
+});
+
